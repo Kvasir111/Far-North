02bfa4a62973cff35d0454e9887c6d7187ed1016
varangian111 <rrowe222@gmail.com>
2019-12-24 14:26:02 -0600

    centered name input and installed tailwind custom forms

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="h-screen flex">
-    <div class="bg-white w-1/3 p-4 rounded m-auto">
-      <h1 class="text-center text-xl mb-2">Getting Started</h1>
-      <div id="name" class="block">
+    <div class="bg-white w-full md:w-1/3 lg:w-2/3 p-4 rounded m-auto">
+      <h1 class="text-center text-xl mb-2">Character Generation</h1>
+      <div id="name" class="block text-center">
       <label for="characterName">Character Name Entry</label>
       <input type="text" v-model="characterName" id="characterName" placeholder="Enter Name Here" class="text-center border-b-2 border-red-700 px-4 py-2">
       <button type="button" v-on:click="generateName">Generate Name</button>
@@ -10,13 +10,13 @@
       <div id="race" class="block">
         <h2 class="text-center border-b-2 border-red-700">Select Race</h2>
         <ul :key="index" v-for="(race, index) in races">
-          <li><input type="radio" name="raceSelection" v-bind:value="race">{{race}}</li>
+          <li><input class="form-radio m-2" type="radio" name="raceSelection" v-bind:value="race">{{race}}</li>
         </ul>
       </div>
       <div id="homeland" class="block">
         <h2 class="text-center border-b-2 border-red-700">Select Homeland</h2>
         <ul :key="index" v-for="(land, index) in homeland">
-          <li><input type="radio" name="homelandSelection" v-bind:value="land">{{land}}</li>
+          <li><input class="m-2 form-radio" type="radio" name="homelandSelection" v-bind:value="land">{{land}}</li>
         </ul>
       </div>
     </div>
